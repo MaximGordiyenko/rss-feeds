cddc535d024b19e1ab00f432b1d46f3f83d2e1bf
MaximGordiyenko <maxim.gordiyenko@gmail.com>
2023-07-29 16:32:56 +0300

    fix: remove comments

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -1,22 +1,8 @@
-import { Outlet, useNavigate, useLocation } from "react-router-dom";
+import { Outlet} from "react-router-dom";
 import { Main } from "../main/Main.jsx";
 import { NavBar } from "../NavBar.jsx";
-import { useState, useEffect } from "react";
-import { useCookies } from "react-cookie";
 
 export const Layout = () => {
-  const [values, setValues] = useState({ email: "", password: "" });
-  // const [cookies] = useCookies(["jwt"]);
-  // const navigate = useNavigate();
-  // let location = useLocation();
-  //
-  // useEffect(() => {
-  //   if (cookies.jwt) {
-  //     navigate("/");
-  //   }
-  // }, [cookies, navigate, location]);
-  //
-  // console.log(location.pathname);
   return (
     <Main>
       <NavBar/>
